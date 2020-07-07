0eff5fa40250b702bd6485fed39938aa374f25a6
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2020-07-07 20:58:39 +0800

    Update NormalMapperTest.cs

--- test/UTProject/NormalMapperTest.cs
+++ test/UTProject/NormalMapperTest.cs
@@ -12,7 +12,7 @@ namespace UTProject
     [Trait("映射测试", "普通类")]
     public class NormalMapperTest
     {
-        [Fact(DisplayName = "字段映射测试")]
+        [Fact(DisplayName = "字段映射类型转换测试")]
         public void Test1()
         {
             //MapperBuilder<NormalSrcFieldModel,NormalDstFieldModel>.
@@ -30,7 +30,7 @@ namespace UTProject
         }
 
 
-        [Fact(DisplayName = "字段映射测试")]
+        [Fact(DisplayName = "字段映射忽略大小写测试")]
         public void Test2()
         {
             MapperBuilder<NormalSrcFieldModel, NormalDstFieldModel>
@@ -52,25 +52,18 @@ namespace UTProject
         }
 
 
-        [Fact(DisplayName = "字段映射测试")]
+        [Fact(DisplayName = "字段映射前缀测试")]
         public void Test3()
         {
+
             MapperBuilder<NormalSrcFieldModel, NormalDstFieldModel>
                 .Create()
-                .IgnoreCase()
+                .SetPrefix("td_")
                 .Compile();
             NormalSrcFieldModel src = new NormalSrcFieldModel();
-            var date = DateTime.Now;
-            src.Date = date.ToString();
             var dst = LyciumMapper<NormalDstFieldModel>.MapperFrom(src);
-            Assert.Equal(src.Name, dst.name);
-            Assert.Equal(src.Age, dst.Age);
-            Assert.Equal(date.Year, dst.Date.Year);
-            Assert.Equal(date.Month, dst.Date.Month);
-            Assert.Equal(date.Day, dst.Date.Day);
-            Assert.Equal(date.Hour, dst.Date.Hour);
-            Assert.Equal(date.Minute, dst.Date.Minute);
-            Assert.Equal(date.Second, dst.Date.Second);
+            Assert.Equal(src.ida.ToString(), dst.td_ida);
+            
         }
 
 
